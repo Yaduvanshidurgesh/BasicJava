4db989904d38da2b5afb2f56203890689a016b3f
Yaduvanshidurgesh <104846676+Yaduvanshidurgesh@users.noreply.github.com>
2023-06-10 20:11:01 +0530

    Update JavaBasic2.java

--- src/test/java/ProjectJava/JavaBasic2.java
+++ src/test/java/ProjectJava/JavaBasic2.java
@@ -5,6 +5,8 @@ public class JavaBasic2 {
 	private void test(){
 		// TODO Auto-generated method stub
        System.out.println("test1");
+		System.out.println("test1");System.out.println("test1");System.out.println("test1");
+		
 	}
 	
 }
